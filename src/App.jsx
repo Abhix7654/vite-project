395d1f05e57c2ad44c76b03dc5c4cafa43a2084a
abhishek <rajabhishek7654@gmail.com>
2024-09-27 09:29:07 +0530

    VITE-PROJECT

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+<h1 className='h-10 w-20 bg-red-700 mt-10 ml-10 rounded text-4xl'>hello</h1>
+
+<h1 className='text-5xl'>asdfg {import.meta.env.VITE_API_KEY}</h1>
+    </div>
+  )
+}
+
+
+export default App
