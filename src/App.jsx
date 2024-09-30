b30f1da23815ab16547419e8477c6453a07fcf31
abhishek <rajabhishek7654@gmail.com>
2024-09-30 09:01:30 +0530

    element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 function App() {
+
+  let homepage="Abhishek";
+
   return (
     <div>
 
 <h1 className='h-10 w-20 bg-red-700 mt-10 ml-10 rounded text-4xl'>hello</h1>
 
 <h1 className='text-5xl'>asdfg {import.meta.env.VITE_API_KEY}</h1>
+<h1 className='h-20px  bg-yellow-700 text-5xl'>{homepage}</h1>
     </div>
   )
 }
