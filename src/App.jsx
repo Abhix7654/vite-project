4f2b6d81cd24239bd5bdfc3ab524b145b9ebbf43
abhishek <rajabhishek7654@gmail.com>
2024-09-30 09:10:37 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 function App() {
 
   let homepage="Abhishek";
+  let subject=["Maths","Science","English","Hindi"];
+  let tags=[<h1>hey</h1>,<h2>boy</h2>];
+  let profile={
+    name:"Muskan",
+    age:20,
+    city:"Delhi",
+    isEmployed:true
+  }
 
   return (
     <div>
@@ -11,6 +19,12 @@ function App() {
 
 <h1 className='text-5xl'>asdfg {import.meta.env.VITE_API_KEY}</h1>
 <h1 className='h-20px  bg-yellow-700 text-5xl'>{homepage}</h1>
+
+<h1>{subject}</h1>
+<h1>{tags}</h1>
+<h1 className='text-5xl'>{profile.name}</h1>
+
+
     </div>
   )
 }
